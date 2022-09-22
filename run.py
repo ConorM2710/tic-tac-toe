e9e7d7ceb5f6677e2a940bcd0c80bc6cb38c15c1
ConorM2710 <conormoyles@outlook.com>
2022-09-22 13:16:06 +0000

    Add more global variables for game, user, winner and running the game

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#-----Global Variables-----
+currentUser = "X"
+winner = None
+gameRunning = True
+
+
+# Layout of the 3 x 3 game board
+game_board = [
+    "?", "?", "?",
+    "?", "?", "?",
+    "?", "?", "?"
+]
+
+
+
+# displaying the game board to the console
+def show_board():
+    """
+    Lays out the grid and 0 index system 
+    """
+    print(game_board[0] + " | " + game_board[1] + " | " + game_board[2])
+    print("--+---+--")
+    print(game_board[3] + " | " + game_board[4] + " | " + game_board[5])
+    print("--+---+--")
+    print(game_board[6] + " | " + game_board[7] + " | " + game_board[8])
+
+
+# takes the players input
+
+
+
+show_board()
