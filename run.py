2446365192dc9fae1e0bc970a328296b96b4122f
ConorM2710 <conormoyles@outlook.com>
2022-09-26 10:53:53 +0000

    Add the rows and columns functions that check for win, diagonals will be done next

--- run.py
+++ run.py
@@ -36,7 +36,46 @@ def user_input(board):
     else:
         print("Something went wrong, please enter a valid integer from 1-9")
 
-# checking for the winner
+# checking for the winner or a draw, across all three directions
+# Rows
+
+
+def check_rows(board):
+    """
+    Checking the horizontals to see if there has been a win
+    """
+    global WINNER
+    if board[0] == board[1] == board[2] and board[1] != "?":
+        WINNER = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != "?":
+        WINNER = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != "?":
+        WINNER = board[6]
+        return True
+
+
+# Columns
+
+
+def check_columns(board):
+    """
+    Checking the verticals to see if there has been a win, utilising
+    the same method as the check_rows
+    """
+    global WINNER
+    if board[0] == board[3] == board[6] and board[0] != "?":
+        WINNER = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != "?":
+        WINNER = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != "?":
+        WINNER = board[2]
+        return True
+    
+
 
 
 while GAMERUNNING:
